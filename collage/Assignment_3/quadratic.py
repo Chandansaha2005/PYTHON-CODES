09e491f7286302a7bacbeb80f2aa8c25e0be0da6
Chandan Saha <chandansaha1945@gmail.com>
2025-07-26 23:09:15 +0530

    done

--- collage/Assignment_3/quadratic.py
+++ collage/Assignment_3/quadratic.py
@@ -8,3 +8,12 @@ d=b*b-4*a*c
 if(a!=0):
     if(d>0):
         r1=(-b+math.sqrt(d))/(2*a)
+        r2=(-b-math.sqrt(d))/(2*a)
+        print("Real and Different Roots = ",r1,"and",r2)
+    elif(d==0):
+        root = -b/(2*a)
+        print("Real an Equal Roots = ", root)
+    else:
+        print("Roots are Imaginary")
+else:
+    print("Not a Quadratic Equation")
