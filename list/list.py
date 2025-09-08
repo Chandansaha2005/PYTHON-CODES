225b4f745446fab6e1be97b229ec627ab16d8fa7
Chandan Saha <chandansaha1945@gmail.com>
2025-09-08 12:42:37 +0530

    revising lists

---
+++ list/list.py
@@ -0,0 +1,9 @@
+hero=["ben","spider man","iron man"]
+marks=[10.5,85.1,4,2,900]
+print(hero)
+print(marks)
+datatypes=[10,15.2,"chandan",'r',True]
+print(datatypes)
+print(type(hero))
+print(type(marks))
+print(type(datatypes))
